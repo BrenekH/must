4b85cca67ed501cd69645b0502ead552c5bd6c50
Brenek Harrison <brenekharrison@gmail.com>
2021-07-09 15:24:22 -0600

    Define commands

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	// Commands
+	//   - update - Refresh known package versions
+	//   - upgrade - Upgrades packages
+	//   - install - Install new packages
+
 	fmt.Println("Hello, World!")
 }
