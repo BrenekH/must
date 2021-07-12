cfba1af1e66a22ba33d2d6d9ee47ee5305da03b0
Brenek Harrison <brenekharrison@gmail.com>
2021-07-12 16:19:12 -0600

    Basic Upgrade functionality

--- upgrade.go
+++ upgrade.go
@@ -1,10 +1,36 @@
 package myaurhelper
 
-import "errors"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func Upgrade(ac AppConfig) error {
-	// TODO: Identify all installed packages
-	// TODO: Check installed versions against local PKGBUILD
-	// TODO: If version is out-of-date, run makepkg and install
-	return errors.New("not implemented")
+	pkgs, err := ac.DS.KnownPackages()
+	if err != nil {
+		return fmt.Errorf("getting known packages from database: %v", err)
+	}
+
+	for _, pkg := range pkgs {
+		if !pkg.UpdateAvailable {
+			continue
+		}
+
+		fmt.Printf("Upgrading %v\n", pkg.Name)
+
+		// TODO: Show git diff of PKGBUILD
+
+		cmd := exec.Command("makepkg", "-si")
+		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+		cmd.Dir = ac.AppDir + "/" + pkg.Name
+
+		if err = cmd.Run(); err != nil {
+			return fmt.Errorf("makepkg: %v", err)
+		}
+
+		fmt.Printf("%v upgraded\n", pkg.Name)
+	}
+
+	return nil
 }
