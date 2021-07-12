8f840430aa3ee3790dda5d6c88f0554820fc89d5
Brenek Harrison <brenekharrison@gmail.com>
2021-07-12 11:18:50 -0600

    Update implementation

--- update.go
+++ update.go
@@ -1,8 +1,43 @@
 package myaurhelper
 
-import "errors"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
 
-func Update() error {
-	// TODO: Download updates for PKGBUILDs and other files from the AUR
-	return errors.New("not implemented")
+func Update(ac AppConfig) error {
+	pkgs, err := ac.DS.KnownPackages()
+	if err != nil {
+		return err
+	}
+
+	for _, pkg := range pkgs {
+		pkgDir := fmt.Sprintf("%v/%v", ac.AppDir, pkg.Name)
+
+		cmd := exec.Command("git", "pull")
+
+		// Connect user to git process so they know what is going on
+		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+
+		// Run git pull in the package directory instead of the current working directory
+		cmd.Dir = pkgDir
+
+		b, err := cmd.Output()
+		if err != nil {
+			return fmt.Errorf("git pull: %v", err)
+		}
+
+		if strings.TrimSpace(string(b)) == "Already up to date." {
+			return nil
+		}
+
+		pkg.UpdateAvailable = true
+		if err = ac.DS.UpdatePackage(pkg); err != nil {
+			return fmt.Errorf("update database: %v", err)
+		}
+	}
+
+	return nil
 }
